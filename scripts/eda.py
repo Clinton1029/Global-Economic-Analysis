95dcc705dc97a7d8ed1521eaaffb0ee9b3db583e
clinton yade <yadeclinton20@gmail.com>
2025-03-04 06:27:01 +0300

    load the daset

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,1 +1,8 @@
+import pandas as pd
+
+# Load the dataset
+file_path = "../data/world_economic_indicators.csv"  # Change this to the correct file path
+df = pd.read_csv(file_path)
+
 # Exploratory Data analysis
+
